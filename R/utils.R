574d5d76bbc3b31bb12e32cbaed97f4470a94cd0
lnferreira <ferreira@leonardonascimento.com>
2022-03-28 20:39:34 +0200

    migrating some useful function from ts_dist.R to utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,45 @@
+#' Normalize a distance/similarity matrix.
+#'
+#' @param D Distance/similarity matrix
+#' @param to An array of two elements c(min_value, max_value) representing
+#'    the interval where the elements of dist_matrix will be normalized to.
+#'
+#' @return Normalized matrix
+#' @importFrom scales rescale
+#' @export
+dist_matrix_normalize <- function(D, to=c(0,1)) {
+    distNorm = matrix(0, nrow(D), ncol(D))
+    d = D[upper.tri(D)]
+    d = rescale(d, to = to)
+    distNorm[upper.tri(distNorm)] = d
+    distNorm = distNorm + t(distNorm)
+    colnames(distNorm) = colnames(D)
+    rownames(distNorm) = rownames(D)
+    distNorm
+}
+
+
+#' Returns the distance value that corresponds to the desired percentile. This function
+#' is useful when the user wants to generate networks with different distance functions
+#' but with the same link density.
+#'
+#' @param D distance matrix
+#' @param percentile (Float) The desired percentile of lower distances.
+#' @param is_D_symetric (Boolean)
+#'
+#' @return Distance percentile value.
+#' @export
+dist_percentile <- function(D, percentile = 0.1, is_D_symetric=TRUE) {
+    D[is.na(D)] = +Inf
+    d = D
+    if (is_D_symetric){
+        d = D[upper.tri(D)]
+    } else {
+        d = D[upper.tri(D) | lower.tri(D)]
+    }
+    quantile(d, probs = c(percentile))
+}
+
 
 #' Extract events from a time series.
 #'
@@ -42,16 +84,16 @@ events_from_ts <- function(ts, th, method=c("greater_than", "lower_than",
                events_ts[ts <= th] = 1
                },
            top_percentile = {
-               events_ts[ts >= quantile(ts, probs = 1 - th)]
+               events_ts[ts >= quantile(ts, probs = 1 - th)] = 1
            },
            lower_percentile = {
-               events_ts[ts >= quantile(ts, probs = th)]
+               events_ts[ts >= quantile(ts, probs = th)] = 1
            },
            highest = {
-               events_ts[order(ts1, decreasing = T)[1:th]]
+               events_ts[order(ts, decreasing = T)[1:th]] = 1
            },
            lowest = {
-               events_ts[order(ts1)[1:th]]
+               events_ts[order(ts)[1:th]] = 1
            })
     events_ts
 }
