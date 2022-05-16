cf02dfff82f95fa69bd7e33d88a1247fe9df96cb
lnferreira <ferreira@leonardonascimento.com>
2022-05-16 18:00:52 +0200

    adding tsnet_vg()

---
+++ R/ts_to_net.R
@@ -0,0 +1,48 @@
+
+
+#' Construct the visibility graph from a time series
+#'
+#' @param x Array. Time series
+#' @param method String. Construction method: "nvg" (default) for
+#'   Natural visibility graph, "hvg" horizontal visibility graph.
+#' @param num_cores Number of cores (default = 1).
+#'
+#' @return visibility graph
+#' @export
+tsnet_vg <- function(x, method=c("nvg", "hvg"), num_cores=1) {
+    id_combs = combn(length(x), 2, simplify = F)
+    method = match.arg(method)
+    links = unlist(mclapply(id_combs, \(ids){
+        switch(method,
+               nvg={
+                   linked = FALSE
+                   if (abs(diff(ids))==1) {
+                       linked = TRUE
+                    } else {
+                        for (i in seq(ids[1]+1, ids[2]-1)) {
+                            if (x[i] >= x[ids[2]] + ((x[ids[1]]-x[ids[2]])*(ids[2]-i)/(ids[2]-ids[1]))) {
+                                linked = FALSE
+                                break
+                            }
+                        }
+                    }
+                    linked
+                },
+                hvg = {
+                    linked = FALSE
+                    if (abs(diff(ids))==1) {
+                        linked = TRUE
+                    } else {
+                        for (i in seq(ids[1]+1, ids[2]-1)) {
+                            if (x[i]>=x[ids[1]] | x[i]>=x[ids[2]]) {
+                                linked = FALSE
+                                break
+                            }
+                        }
+                    }
+                    linked
+                })
+    }, mc.cores = num_cores))
+    links = do.call(rbind, id_combs[links])
+    graph.data.frame(links, directed = F)
+}
