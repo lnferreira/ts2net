7e91aad7a92ddd9b8b184990bd080bcead4b1a26
lnferreira <ferreira@leonardonascimento.com>
2022-05-23 14:10:22 +0200

    Adding tsnet_rn()

--- R/ts_to_net.R
+++ R/ts_to_net.R
@@ -38,3 +38,37 @@ tsnet_vg <- function(x, method=c("nvg", "hvg"), num_cores=1) {
     links = do.call(rbind, id_combs[links])
     graph.data.frame(links, directed = F)
 }
+
+
+#' Construct the recurrence network from a time series.
+#'
+#' This function constructs the recurrence matrix of the time
+#' series using the function `rqa()` from \pkg{nonlinearTseries}
+#' package.
+#'
+#' @param x Array. Time series
+#' @param radius Maximum distance between two phase-space
+#'   points to be considered a recurrence.
+#' @param embedding.dim Integer denoting the dimension in which
+#'   we shall embed the time.series.
+#' @param time.lag Integer denoting the number of time steps that
+#'   will be use to construct the Takens' vectors.
+#' @param do.plot Boolean. Show recurrence plot (default = FALSE)
+ #' @param ... Other parameters to `rqa()` from \pkg{nonlinearTseries}
+#'
+#' @return recurrence network
+#' @importFrom nonlinearTseries rqa estimateEmbeddingDim
+#' @export
+tsnet_rn <- function(x, radius, embedding.dim, time.lag=1, do.plot = FALSE, ...) {
+    if (missing(embedding.dim))
+        embedding.dim = estimateEmbeddingDim(time.series = x,
+                                             time.lag = time.lag,
+                                             do.plot = FALSE)
+    rm = rqa(time.series = x, radius = radius, embedding.dim = embedding.dim ,
+             do.plot=do.plot, ...)$recurrence.matrix
+    net = graph.adjacency(as.matrix(rm), mode = "undirected")
+    net$embedding_dim = embedding.dim
+    net$time_lag = time.lag
+    net$radius = radius
+    simplify(net)
+}
